89d131440318ced7fa582a8760c392a2b0d324b7
kohei3110 <kk.31108424@gmail.com>
2025-03-21 13:52:57 +0900

    refactor: remove unused imports from App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { CssBaseline, ThemeProvider, createTheme } from '@mui/material'
 import { RealtimeTranslation } from './features/realtime-translation/RealtimeTranslation'
